0a1092a1ef5b2e79ae054cf1c44b8df9916c5d74
VladmiralKu <89991003706@mail.ru>
2026-06-13 09:43:41 +0300

    debug tx lookup

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -595,6 +595,9 @@ async def msg_edit_tx_id(message: Message, state: FSMContext):
            WHERE t.id = %s AND t.user_id = %s""",
         (tx_id, message.from_user.id)
     )
+    # debug
+    all_txs = await fetchall("SELECT id, user_id FROM transactions WHERE id = %s", (tx_id,))
+    await message.answer("DEBUG tx: " + str(all_txs))
     if not tx:
         await message.answer("Транзакция #" + str(tx_id) + " не найдена для user " + str(message.from_user.id))
         await state.clear()
